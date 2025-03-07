f7d777c80f13c5555f81db985dad0690ea2a9c82
rifki <rifkibahrululum@gmail.com>
2025-03-08 04:29:50 +0700

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/portofolio/", // Ganti 'my-porto' dengan nama repository GitHub-mu
-  plugins: [react()],
-})
